f02ea1bee1edc6a1e1cee9f2652fdcfaacd14a3e
yuchou87 <yuchou87@gmail.com>
2019-08-03 08:59:21 +0800

    [A] todo页面和组件模块

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import TodoList from "./pages/Todolist";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div className="Todo-app">
+        <TodoList />
+      </div>
     </div>
   );
 }
---
+++ web/src/pages/Todolist.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "../App.css";
+import TodoForm from "../components/TodoForm";
+import Todo from "../components/Todo";
+
+function TodoList() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  return (
+    <div className="todo-list">
+      {todos.map((todo, index) => (
+        <Todo
+          key={index}
+          index={index}
+          todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+        />
+      ))}
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
+}
+
+export default TodoList;
