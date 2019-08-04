52c1247810354bd4af39924d9374427bd92d7686
yuchou <yuchou87@gmail.com>
2019-08-04 22:23:02 +0800

    [U] 绑定限制

--- app/models.go
+++ app/models.go
@@ -5,7 +5,7 @@ import "time"
 // 数据模型
 type Todo struct {
 	ID          uint      `gorm:"primary_key;AUTO_INCREMENT;not null" json:"id"`
-	Content     string    `gorm:"type:text" json:"content"`
+	Content     string    `gorm:"type:text" json:"content" binding:"required"`
 	IsCompleted bool      `gorm:"default:false" json:"is_completed"`
 	CreatedAt   time.Time `gorm:"default:null" json:"created_at"`
 	UpdatedAt   time.Time `gorm:"default:null" json:"updated_at"`
